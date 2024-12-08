4bc99e3de7083fcfc3e7922344dbfc36dba539e1
BlackCheppun <aghouilesilyes1@gmail.com>
2024-12-08 02:49:51 +0100

    removed unecessary imports

--- app/src/main/java/com/mobilecomputing/trackandtaste/MainActivity.java
+++ app/src/main/java/com/mobilecomputing/trackandtaste/MainActivity.java
@@ -1,18 +1,12 @@
 package com.mobilecomputing.trackandtaste;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mobilecomputing.trackandtaste.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
