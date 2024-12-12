7cb7a1408b94eb86ca3ac7ca0e54353f14169981
BlackCheppun <aghouilesilyes1@gmail.com>
2024-12-12 20:33:30 +0100

    implemented basic map init, has centerlocation overlay, scale overlay, compass overlay

--- app/src/main/java/com/mobilecomputing/trackandtaste/MapFragment.java
+++ app/src/main/java/com/mobilecomputing/trackandtaste/MapFragment.java
@@ -1,20 +1,110 @@
 package com.mobilecomputing.trackandtaste;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapFragment extends Fragment {
+
+    private MapView mapView;
+    private MyLocationNewOverlay locationoverlay;
+    private FloatingActionButton centerLocationB;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        Tools.askPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, getContext(), getActivity());
+
+        Configuration.getInstance().setUserAgentValue(requireContext().getPackageName());
+        mapView = view.findViewById(R.id.mapView);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+
+        // Add location overlay
+        locationoverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getContext()),mapView);
+        locationoverlay.enableMyLocation();
+        mapView.getOverlays().add(locationoverlay);
+
+        // add compassoverlay
+        CompassOverlay compassOverlay = new CompassOverlay(getContext(), new InternalCompassOrientationProvider(getContext()), mapView);
+        compassOverlay.enableCompass();
+
+        // add the sclare bar overlay
+        ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(mapView);
+        scaleBarOverlay.setAlignBottom(true);
+
+
+        mapView.getOverlays().add(compassOverlay);
+        mapView.getOverlays().add(scaleBarOverlay);
+        centerOnUserLocation();
+
+        centerLocationB = view.findViewById(R.id.center_locationB);
+        centerLocationB.setOnClickListener(v ->{
+            centerOnUserLocation();
+        });
+
+
+
+        return view;
+
     }
+
+
+    private void centerOnUserLocation() {
+
+        if (locationoverlay.getMyLocation() != null) {
+            IMapController mapController = mapView.getController();
+            mapController.setZoom(15.0);
+            mapController.setCenter(locationoverlay.getMyLocation());
+        } else {
+            Toast.makeText(requireContext(), "Unable to determine location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+
 }
